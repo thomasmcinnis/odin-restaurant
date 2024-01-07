32cbd1db6a0795586ecf782850937ab11492e5e1
Thomas McInnis <hey@thomasmcinnis.com>
2024-01-07 12:36:31 +1100

    Add toggle between views

--- src/index.js
+++ src/index.js
@@ -5,8 +5,29 @@ import getHome from './components/home/homePage';
 const homeView = getHome();
 const menuView = getMenu();
 
+let isHome = true;
 
 // where we selectively render content
 const content = document.getElementById('content');
+content.appendChild(isHome ? homeView : menuView);
 
-content.appendChild(menuView);
+// Function to remove current content... content, and replace with new.. content
+function replaceContent() {
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+
+    isHome = !isHome;
+
+    content.appendChild(isHome ? homeView : menuView);
+}
+
+// Create a button to switch between menu and home
+const toggleButton = document.createElement('button');
+toggleButton.textContent = 'Toggle Home and Welcome Page';
+
+toggleButton.addEventListener('click', replaceContent);
+
+const body = document.body;
+
+body.insertBefore(toggleButton, content);
